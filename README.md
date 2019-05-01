1610843e78335dbc503e50ca66e470fa7f5b8cfa
roy <newroyker@gmail.com>
2019-04-30 23:21:49 -0700

    repro

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # vintage
 Apache Spark `DStreams` playground
 
-# Run 
+# Run (in order on separate shell) 
 * `nc -lk 9999`
 * `sbt run`
+
+# Output
+Accumulators report following:
+```text
+RDD size: 0
+------------------------------------------------------------
+LongAccumulator(id: 0, name: Some(test-timer), value: 1284633)
+LongAccumulator(id: 1, name: Some(fm-timer), value: 0)
+LongAccumulator(id: 2, name: Some(m-timer), value: 0)
+LongAccumulator(id: 3, name: Some(rbk-timer), value: 0)
+------------------------------------------------------------
+RDD size: 33
+------------------------------------------------------------
+LongAccumulator(id: 0, name: Some(test-timer), value: 1278166)
+LongAccumulator(id: 1, name: Some(fm-timer), value: 53805)
+LongAccumulator(id: 2, name: Some(m-timer), value: 292601)
+LongAccumulator(id: 3, name: Some(rbk-timer), value: 153335)
+------------------------------------------------------------
+RDD size: 0
+------------------------------------------------------------
+LongAccumulator(id: 0, name: Some(test-timer), value: 1081836)
+LongAccumulator(id: 1, name: Some(fm-timer), value: 0)
+LongAccumulator(id: 2, name: Some(m-timer), value: 0)
+LongAccumulator(id: 3, name: Some(rbk-timer), value: 0)
+------------------------------------------------------------
+```
+
+Tasks' `Duration` is 1, 10, 1 ms respectively.
--- src/main/scala/org/apache/spark/streaming/Demo.scala
+++ src/main/scala/org/apache/spark/streaming/Demo.scala
@@ -1,10 +1,10 @@
 package org.apache.spark.streaming
 
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.SparkConf
 import org.apache.spark.internal.Logging
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.util.LongAccumulator
+import org.apache.spark.{SparkConf, SparkContext}
 
 object Demo extends App with Logging {
 
@@ -14,33 +14,57 @@ object Demo extends App with Logging {
     Logger.getRootLogger.setLevel(Level.WARN)
   }
 
-  val sparkConf = new SparkConf().setMaster("local[2]").setAppName("Demo")
-  val ssc = new StreamingContext(sparkConf, Seconds(1))
+  val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Demo")
+  val sc = new SparkContext(sparkConf)
+  val ssc = new StreamingContext(sc, Seconds(30))
 
-  def meter[T](accum: LongAccumulator)(block: => T): T = {
+  val timers: Map[String, LongAccumulator] =
+    Seq("test-timer", "fm-timer", "m-timer", "rbk-timer")
+      .map(t => t -> sc.longAccumulator(t))
+      .toMap
+
+  def meter[T](timerName: String)(block: => T): T = {
     val start = System.nanoTime()
     val result = block
-    accum.add(System.nanoTime() - start)
-    accum.value
+    timers(timerName).add(System.nanoTime() - start)
     result
   }
 
-
-  val timer = ssc.sparkContext.longAccumulator("Timer")
-
   ssc
     .socketTextStream("localhost", 9999, StorageLevel.MEMORY_AND_DISK_SER)
-    .flatMap(_.split(" "))
-    .map(x => (x, 1))
+    .transform { rdd =>
+      timers.values.foreach(_.reset())
+      rdd
+    }
+    .transform { rdd =>
+      meter("test-timer") {
+        Thread.sleep(1)
+      }
+      rdd
+    }
+    .flatMap { x =>
+      meter("fm-timer") {
+        x.split(" ")
+      }
+    }
+    .map { x =>
+      meter("m-timer") {
+        (x, 1)
+      }
+    }
     .reduceByKey {
       (a, b) =>
-        meter(timer) {
+        meter("rbk-timer") {
           a + b
         }
     }
-    .print()
+    .foreachRDD { r =>
+      println(s"RDD size: ${r.count()}")
+      println("------------------------------------------------------------")
+      timers.values.foreach(println)
+      println("------------------------------------------------------------")
+    }
 
   ssc.start()
   ssc.awaitTermination()
-
 }
